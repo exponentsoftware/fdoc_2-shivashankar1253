6b0dd147895ba3c0b6e7d9f71e073d89545bb5c7
shivashankar1253 <65164540+shivashankar1253@users.noreply.github.com>
2021-03-04 10:51:25 +0530

    Add files via upload

---
+++ countries.js
@@ -0,0 +1,16 @@
+let countries = [
+    'ALBANIA',
+    'BOLIVIA',
+    'CANADA',
+    'DENMARK',
+    'ETHIOPIA',
+    'FINLAND',
+    'GERMANY',
+    'HUNGARY',
+    'IRELAND',
+    'JAPAN',
+    'KENYA'
+  ];
+  countries.map((data,index)=>{
+        console.log([data.toLowerCase(), data.substring(0,3), data.length])
+  })
